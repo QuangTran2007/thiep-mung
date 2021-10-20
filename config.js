de282d88946d1abac4a42fbbb5af5852d0ff6a5a
QuangTran2007 <92450546+QuangTran2007@users.noreply.github.com>
2021-10-20 21:56:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const eventName = 'Chúc Mừng 20-10 nha Phương';
+const titleCard = 'Quăng vô mặt mày ít quà';
+const contentCard = 'Chúc 20/10 vui vẻ . cho xin 5k luôn nha';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
